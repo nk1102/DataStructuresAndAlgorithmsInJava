589fe96ecf6b9472147066d06e5c8cb3dc6a40b6
nikunj <nikunj.khandelwal_cs19@gla.ac.in>
2021-03-28 12:58:49 +0530

    created generic interface ListADT to declare all the methods of the singly linked list class

---
+++ src/LinkedList/Myinterface/ListADT.java
@@ -0,0 +1,10 @@
+package LinkedList.Myinterface;
+
+/**
+     * created an interface ListADT to  declare all the methods used fpr the implementation
+     * singly linked list.
+     */
+
+    public interface ListADT<E> {
+
+}
