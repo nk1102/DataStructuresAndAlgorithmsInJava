c26cea9228b088d3c1d447b4d780ec389eff98f8
nikunj <nikunj.khandelwal_cs19@gla.ac.in>
2021-03-28 13:08:53 +0530

    added getter and setter methods for the Node class

--- src/LinkedList/Myinterface/Implementation/Node.java
+++ src/LinkedList/Myinterface/Implementation/Node.java
@@ -17,7 +17,22 @@ public class Node<E>{
 
     public Node(E data) {
         this.data = data;
-        next=null;
+        next = null;
     }
 
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
 }
